2aec910dbb536fe4d8da3ab9ea0d8bad68357a7b
mayurk11 <166540933+mayurk11@users.noreply.github.com>
2026-04-02 22:58:20 +0530

    Add: set-zeroes-in-matrix - submission-0

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    
+    def mark_inf(self,matrix,row,col):
+        r = len(matrix)
+        c = len(matrix[0])
+
+        for i in range(0,r):
+            if matrix[i][col] != 0 :
+                matrix[i][col] = float("inf")
+        
+        for j in range(0,c):
+            if matrix[row][j] !=0 :
+                matrix[row][j] =float("inf")
+    
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        r = len(matrix)
+        c = len(matrix[0])
+
+        for i in range(0,r):
+            for j in range(0,c):
+                if matrix[i][j] == 0:
+                    self.mark_inf(matrix,i,j)
+
+        for i in range(0,r):
+            for j in range(0,c):
+                if matrix[i][j] == float('inf'):
+                    matrix[i][j] = 0
+        
+    
+    
