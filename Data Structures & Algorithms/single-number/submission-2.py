5c31e02f41f1d31b7865e40411b1ffa0bd181b56
mayurk11 <166540933+mayurk11@users.noreply.github.com>
2026-04-17 23:10:02 +0530

    Add: single-number - submission-2

---
+++ Data Structures & Algorithms/single-number/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # for i in range(len(nums)):
+        #     flag = True
+        #     for j in range(len(nums)):
+        #         if i !=j and nums[i] == nums[j]:
+        #             flag = False
+        #             break
+        #     if flag:
+        #         return nums[i]
+        
+        hash_map = {}
+        for num in nums:
+            hash_map[num] = hash_map.get(num,0)+1
+
+        for key in hash_map:
+            if hash_map[key] == 1:
+                return key 
