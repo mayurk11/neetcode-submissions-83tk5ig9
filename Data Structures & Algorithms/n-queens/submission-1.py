515b8eceb05fe5178e5de289d3615f113d59cd9f
mayurk11 <166540933+mayurk11@users.noreply.github.com>
2026-05-10 11:29:24 +0530

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ans  = []
+        board = ["." * n for _ in range(n)]
+        left_row = [0] * n
+        upper_diagonal = [0] * (2 * n - 1)
+        lower_diagonal = [0] * (2 * n - 1)
+
+        
+        self.solve(0, board, ans, left_row, lower_diagonal, upper_diagonal, n)
+        return ans
+        
+
+    def solve(self,col,board, ans, left_row, lower_diagonal, upper_diagonal, n):
+        if col == n:
+            ans.append(list(board))
+            return 
+        
+        for row in range(n):
+            if (left_row[row]== 0
+                and upper_diagonal[row + col] == 0
+                and lower_diagonal[n - 1 + col - row] == 0
+                ):
+
+                left_row[row] = 1
+                upper_diagonal[row + col] = 1
+                lower_diagonal[n - 1 + col - row] = 1
+                board[row] = board[row][:col] + 'Q' + board[row][col+1:]
+
+                self.solve(col+1,board,ans,left_row,lower_diagonal,upper_diagonal,n)
+
+                board[row] = board[row][:col] + "." + board[row][col+1:]
+                left_row[row] = 0
+                upper_diagonal[row + col ] = 0
+                lower_diagonal[n - 1 + col - row] = 0
+
+
+    
